c9ca2675b2309688a3000c82a0db6daf85b7fa1f
elliotlewis8923 <elewis8923@gmail.com>
2014-08-22 12:22:40 +0100

    first test passed

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -10,15 +10,19 @@ describe Airport do
   let(:airport) { Airport.new }
   
   context 'taking off and landing' do
+
     it 'a plane can land' do
+      plane = double (:plane)
+      allow(plane).to receive(:land)
+      airport.land(plane)
     end
     
-    it 'a plane can take off' do
+    xit 'a plane can take off' do
     end
   end
   
   context 'traffic control' do
-    it 'a plane cannot land if the airport is full' do
+    xit 'a plane cannot land if the airport is full' do
     end
     
     # Include a weather condition using a module.
@@ -29,10 +33,10 @@ describe Airport do
     # If the airport has a weather condition of stormy,
     # the plane can not land, and must not be in the airport
     context 'weather conditions' do
-      it 'a plane cannot take off when there is a storm brewing' do
+      xit 'a plane cannot take off when there is a storm brewing' do
       end
       
-      it 'a plane cannot land in the middle of a storm' do
+      xit 'a plane cannot land in the middle of a storm' do
       end
     end
   end
